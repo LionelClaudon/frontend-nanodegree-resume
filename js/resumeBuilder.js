59a7fa5e659a153e774966bf0ad39c0ac03c4e14
lclaudon <lionel.claudon@gmail.com>
2015-08-31 19:33:59 +0200

    no message

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,79 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+var bio = {
+	"name" : "Lionel Claudon",
+	"role" : "Software Engineer",
+	"welcomeMessage" : "Hello, welcome to my Resume",
+	"contacts" : {
+		"email" : "lionel.claudon@gmail.com",
+		"mobile" : "079 781 69 83",
+		"location" : "Lausanne",
+		"github" : "LionelClaudon"
+	},
+	"img" : "images/id.png",
+	"skills" : ["Java", "Spring", "Agile", 1234]
+};
+
+var work = {
+	"job" : [
+	{
+		"employer" : "Sicpa",
+		"title" : "Software Engineer",
+		"years" : "2012 - 2015",
+		"location" : "Lausanne",
+		"description" : "Taxed products marking and tracking"
+	}, 
+	{
+		"employer" : "CES",
+		"title" : "Software Engineer",
+		"years" : "2012",
+		"location" : "Geneva",
+		"description" : "JPEG2000 for embedded video"
+	}
+	]
+};
+
+var education = {
+	"school" : [
+	{
+		"school" : "Grenoble INP",
+		"degree" : "Master",
+		"city" : "Grenoble",
+		"majors" : ["Mathematics"],
+		"years" : "2009 - 2012",
+		"url" : "http://grenoble-inp.com"
+	},
+	{
+		"school" : "IUT Annecy",
+		"degree" : "Technician",
+		"city" : "Annecy",
+		"major" : ["Thermodynamics"],
+		"years" : "2007 - 2009",
+		"url" : "http://annecy.com"
+	}
+	],
+	"onlineCourses" : [
+	{
+		"title" : "Javascript Basics",
+		"school" : "Udacity",
+		"dates" : "2015"
+	}
+	]
+};
+
+var projects = {
+	"project": [
+	{
+		"title" : "Assembly language compiler",
+		"dates" : "2011",
+		"description" : "Compile using C language an assembly file"
+	}
+	]
+};
+
+//$("#workExperience").append(work.name);
+//$("#education").append(education["school"]);
+
+
+
